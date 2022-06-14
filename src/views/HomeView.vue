8437c358dbf82642626ae17892c7c0d5b7bac36d
DESKTOP-NDTG61E\sofien <s.cheikhmohamed@elyosdigital.com>
2022-06-14 17:49:50 +0200

    second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <MenuH />
+    <banner />
     <about-me />
     <portfolio />
     <experience />
@@ -8,12 +10,15 @@
 </template>
 
 <script>
+
+import MenuH from "@/components/global/Menu.vue";
+import Banner from "@/components/global/Banner.vue";
 import AboutMe from "@/components/global/AboutMe.vue";
 import Portfolio from "@/components/global/Portfolio.vue";
 import Experience from "@/components/global/Experience.vue";
 import Contact from "@/components/global/Contact.vue";
 export default {
-  components: { AboutMe, Portfolio, Experience, Contact },
+  components: { MenuH, Banner, AboutMe, Portfolio, Experience, Contact },
   name: "HomeView",
 };
 </script>
